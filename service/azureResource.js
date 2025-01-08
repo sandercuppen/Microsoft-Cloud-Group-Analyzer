a9a5c0c1a1c222d2e705994ba46acbfe8158e1c8
Sander Cuppen <sander.cuppen@gmail.com>
2025-01-08 21:51:04 +0100

    Add parsing of management groups and resource names. Deduplicate nested assignments.

--- service/azureResource.js
+++ service/azureResource.js
@@ -12,6 +12,26 @@ async function init(accessToken, accessTokenAzure, groupID, groupName, tenantID)
         return null
     }
 
+    // Get management groups
+    let mgGroups = await helper.callApi(`https://management.azure.com/providers/Microsoft.Management/managementGroups?api-version=2020-05-01`, accessTokenAzure)
+    const mgLookup = {}
+    if (mgGroups?.value) {
+        mgGroups.value.forEach(mg => {
+            mgLookup[mg.name] = mg.properties?.displayName || mg.name
+        })
+    }
+
+    // Get role definitions for lookup
+    let roleDefinitions = await helper.callApi(`https://management.azure.com/providers/Microsoft.Authorization/roleDefinitions?api-version=2022-04-01`, accessTokenAzure)
+    const roleLookup = {}
+    if (roleDefinitions?.value) {
+        roleDefinitions.value.forEach(role => {
+            roleLookup[role.name] = role.properties?.roleName || role.name
+        })
+    }
+
+    // Use a Set to track unique assignments
+    const uniqueAssignments = new Set()
     let array = []
     let counter = 1
 
@@ -22,16 +42,37 @@ async function init(accessToken, accessTokenAzure, groupID, groupName, tenantID)
             if (roleAssignments) {
                 roleAssignments = roleAssignments?.value?.filter(roleAssignment => roleAssignment?.properties?.principalId == groupID).forEach(res => {
                     if (res.properties.scope.length > 1) { // don't show if scope is empty. If scope is empty, it is the RoleAssignment object. No need to show that again
-                        array.push({
-                            "file": 'azureResource',
-                            "groupID": groupID,
-                            "groupName": groupName,
-                            "service": "Azure Resource",
-                            "resourceID": res.id,
-                            "name": res?.properties?.scope.substring(res?.properties?.scope.lastIndexOf('/') + 1), // take the resource name from the scope
-                            "detailsGroup": `${(groupName.includes('@')) ? 'user' : 'group'} '${groupName}'`,
-                            "details": ``
-                        })
+                        const scope = res?.properties?.scope
+                        let name = scope.substring(scope.lastIndexOf('/') + 1)
+                        
+                        // If this is a management group, use its display name
+                        if (scope.includes('/providers/Microsoft.Management/managementGroups/')) {
+                            const mgId = scope.split('/managementGroups/')[1].split('/')[0]
+                            name = mgLookup[mgId] || mgId
+                        }
+                        
+                        // Get role name from lookup
+                        const roleDefId = res.properties.roleDefinitionId
+                        const roleId = roleDefId.substring(roleDefId.lastIndexOf('/') + 1)
+                        const roleName = roleLookup[roleId] || roleId
+
+                        // Create a unique key for this assignment
+                        const assignmentKey = `${scope}|${roleId}|${groupID}`
+                        
+                        // Only add if we haven't seen this exact assignment before
+                        if (!uniqueAssignments.has(assignmentKey)) {
+                            uniqueAssignments.add(assignmentKey)
+                            array.push({
+                                "file": 'azureResource',
+                                "groupID": groupID,
+                                "groupName": groupName,
+                                "service": "Azure Resource",
+                                "resourceID": res.id,
+                                "name": name,
+                                "detailsGroup": `${(groupName.includes('@')) ? 'user' : 'group'} '${groupName}'`,
+                                "details": `Role: ${roleName}`
+                            })
+                        }
                     }
                 })
             }
@@ -42,9 +83,15 @@ async function init(accessToken, accessTokenAzure, groupID, groupName, tenantID)
     })
 
     return promise.then(() => {
-        return array
+        // Sort the array by scope and role name for better readability
+        return array.sort((a, b) => {
+            // First sort by name (scope)
+            const nameCompare = a.name.localeCompare(b.name)
+            if (nameCompare !== 0) return nameCompare
+            // Then by role name
+            return a.details.localeCompare(b.details)
+        })
     })
 }
-
 
 module.exports = { init }
